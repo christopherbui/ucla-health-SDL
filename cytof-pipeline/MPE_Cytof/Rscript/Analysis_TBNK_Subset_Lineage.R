3be6f0b4bd1d3dc3cd11d31712fa98f485939ef7
Christopher Bui <christopherbui@pm.me>
2025-07-17 16:56:26 -0700

    updates

--- cytof-pipeline/MPE_Cytof/Rscript/Analysis_TBNK_Subset_Lineage.R
+++ cytof-pipeline/MPE_Cytof/Rscript/Analysis_TBNK_Subset_Lineage.R
@@ -10,8 +10,8 @@ rds_files <- list.files(rds_subclust_dir)
 # sce_c2 <- readRDS(file.path(rds_subclust_dir, rds_files[2]))
 # sce_c3c4 <- readRDS(file.path(rds_subclust_dir, rds_files[3]))
 # sce_c5c9 <- readRDS(file.path(rds_subclust_dir, rds_files[4]))
-sce_c6c7 <- readRDS(file.path(rds_subclust_dir, rds_files[5]))
-# sce_c8 <- readRDS(file.path(rds_subclust_dir, rds_files[6]))
+# sce_c6c7 <- readRDS(file.path(rds_subclust_dir, rds_files[5]))
+sce_c8 <- readRDS(file.path(rds_subclust_dir, rds_files[6]))
 
 output_dir <- paste0("D:/CHRISTOPHER_BUI/MPE_CYTOF_RDS/", sel_panel, "/Results_Subcluster")
 
@@ -729,4 +729,138 @@ clust_prop_tbl_pbmc <- prop.table(clust_tbl_pbmc, 2)
 pheatmap(clust_prop_tbl_pbmc, cluster_rows = FALSE, cluster_cols = TRUE, main = "PBMC Samples Cluster Proportions")
 
 
+# C8 ---------------------------------------------------------------------------
+prefix <- "C8"
+
+# DOTPLOTS
+m4_dp <- dotplotTables(sce_c8, "meta4", assay = "exprs", fun = "median", scale = FALSE, q = 0.01)
+m5_dp <- dotplotTables(sce_c8, "meta5", assay = "exprs", fun = "median", scale = FALSE, q = 0.01)
+
+sel_aggregate <- c("median")
+scale_option <- FALSE
+label_for_dotplot <- paste(ifelse(scale_option, "scaled", "non-scaled"), sel_aggregate, "exprs", sep = " ")
+
+# meta4
+tmp_fig <- dotplotFig(m4_dp$expr_long, lab = label_for_dotplot)
+file_name <- paste(sel_panel, prefix, "meta4", "dotplot", sel_aggregate, ifelse(scale_option, "SCALED.png", "NON_SCALED.png"), sep = "_")
+ggsave(filename = file.path(output_dir, file_name), plot = tmp_fig, width = 10, height = 8)
+
+# meta5
+tmp_fig <- dotplotFig(m5_dp$expr_long, lab = label_for_dotplot)
+file_name <- paste(sel_panel, prefix, "meta5", "dotplot", sel_aggregate, ifelse(scale_option, "SCALED.png", "NON_SCALED.png"), sep = "_")
+ggsave(filename = file.path(output_dir, file_name), plot = tmp_fig, width = 10, height = 8)
+
+
+# CORR
+corr_m4 <- cor(m4_dp$expr_wide)
+corr_m5 <- cor(m5_dp$expr_wide)
+
+pheatmap(corr_m4,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Corr between meta4 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+pheatmap(corr_m5,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Corr between meta5 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+
+# meta3 & meta5
+corr_mat_m3_m5 <- cor(m3_dp$expr_wide, m5_dp$expr_wide)
+pheatmap(corr_mat_m3_m5,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Correlation between meta3 and meta5 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+# meta3 & meta6
+corr_mat_m3_m6 <- cor(m3_dp$expr_wide, m6_dp$expr_wide)
+pheatmap(corr_mat_m3_m6,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Correlation between meta3 and meta6 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+# meta3 & meta7
+corr_mat_m3_m7 <- cor(m3_dp$expr_wide, m7_dp$expr_wide)
+pheatmap(corr_mat_m3_m7,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Correlation between meta3 and meta7 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+# meta5 & meta6
+corr_mat_m5_m6 <- cor(m5_dp$expr_wide, m6_dp$expr_wide)
+pheatmap(corr_mat_m5_m6,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Correlation between meta5 and meta6 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+# meta5 & meta7
+corr_mat_m5_m7 <- cor(m5_dp$expr_wide, m7_dp$expr_wide)
+pheatmap(corr_mat_m5_m7,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Correlation between meta5 and meta7 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+# meta6 & meta7
+corr_mat_m6_m7 <- cor(m6_dp$expr_wide, m7_dp$expr_wide)
+pheatmap(corr_mat_m6_m7,
+         cluster_rows = TRUE,
+         cluster_cols = TRUE,
+         main = "Correlation between meta6 and meta7 clusters",
+         fontsize_row = 10,
+         fontsize_col = 10)
+
+
+# ABUNDANCE DOTPLOT
+# meta3
+CATALYST::plotAbundances(sce_c6c7, k = "meta3", by = "cluster_id", group_by = "BATCH") +
+  ggtitle("Abundance by Cluster & Batch")
+# meta5
+CATALYST::plotAbundances(sce_c6c7, k = "meta5", by = "cluster_id", group_by = "BATCH") +
+  ggtitle("Abundance by Cluster & Batch")
+# meta6
+CATALYST::plotAbundances(sce_c6c7, k = "meta6", by = "cluster_id", group_by = "BATCH") +
+  ggtitle("Abundance by Cluster & Batch")
+# meta7
+CATALYST::plotAbundances(sce_c6c7, k = "meta7", by = "cluster_id", group_by = "BATCH") +
+  ggtitle("Abundance by Cluster & Batch")
+
+
+
+# PBMC BATCH SAMPLE
+sce_c6c7_pbmc <- filterSCE(sce_c6c7, tissue_type == "PBMC")
+
+# meta3
+clust_tbl_pbmc <- table(cluster_ids(sce_c6c7_pbmc, "meta3"), colData(sce_c6c7_pbmc)$sample_id)
+clust_prop_tbl_pbmc <- prop.table(clust_tbl_pbmc, 2)
+pheatmap(clust_prop_tbl_pbmc, cluster_rows = FALSE, cluster_cols = TRUE, main = "PBMC Samples Cluster Proportions")
+
+# meta5
+clust_tbl_pbmc <- table(cluster_ids(sce_c6c7_pbmc, "meta5"), colData(sce_c6c7_pbmc)$sample_id)
+clust_prop_tbl_pbmc <- prop.table(clust_tbl_pbmc, 2)
+pheatmap(clust_prop_tbl_pbmc, cluster_rows = FALSE, cluster_cols = TRUE, main = "PBMC Samples Cluster Proportions")
+
+# meta6
+clust_tbl_pbmc <- table(cluster_ids(sce_c6c7_pbmc, "meta6"), colData(sce_c6c7_pbmc)$sample_id)
+clust_prop_tbl_pbmc <- prop.table(clust_tbl_pbmc, 2)
+pheatmap(clust_prop_tbl_pbmc, cluster_rows = FALSE, cluster_cols = TRUE, main = "PBMC Samples Cluster Proportions")
+
+# meta7
+clust_tbl_pbmc <- table(cluster_ids(sce_c6c7_pbmc, "meta7"), colData(sce_c6c7_pbmc)$sample_id)
+clust_prop_tbl_pbmc <- prop.table(clust_tbl_pbmc, 2)
+pheatmap(clust_prop_tbl_pbmc, cluster_rows = FALSE, cluster_cols = TRUE, main = "PBMC Samples Cluster Proportions")
 
