446b3b4694a2fa420c8a0efd42cb80ab982b5b21
Christopher Bui <christopherbui@pm.me>
2025-07-25 17:14:03 -0700

    updates

---
+++ cytof-pipeline/MPE_Cytof/Rscript/Cytokine_Violin_Plot_ALL.R
@@ -0,0 +1,203 @@
+library(cowplot)
+
+# DE tables for each TIER 2 cluster
+DE_files <- list.files(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\Results_Subcluster\Diff_Expr)',
+                       pattern = "\\.txt",
+                       full.names = TRUE)
+
+# RDS files for each TIER 2 cluster
+rds_files <- list.files(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\Results_Subcluster\RDS_Subcluster)',
+                        pattern = "\\.rds",
+                        full.names = TRUE)
+
+# metak for each TIER 2 cluster (used for DE)
+metak <- list(
+  C4 = "meta6",
+  C5 = "meta5",
+  C6 = "meta7",
+  Myeloid = "meta8"
+)
+
+# grouping of TIER 1 clusters
+clust_group <- list(
+  C4 = c(4),
+  C5 = c(5),
+  C6 = c(6),
+  Myeloid = c(1,2,3)
+)
+output_dir <- r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\Results_Subcluster\TEST_VIOLIN)'
+
+
+# ------------------------------------------------------------------------------
+# process SCE
+# sce_main <- readRDS(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\sce_main_with_meta6_full_lin_clusters.rds)')
+# # set pbmc as reference
+# colData(sce_main)$tissue_type <- factor(colData(sce_main)$tissue_type, levels = c("PBMC", "MPE"))
+# # add column denoting PBMC, MPE, or Ref for plotting
+# colData(sce_main)$tissue_type_mod <- case_when(
+#   sce_main$tissue_type == "MPE" ~ "MPE",
+#   sce_main$tissue_type == "PBMC" & sce_main$patient_id != "Ref" ~ "PBMC",
+#   sce_main$tissue_type == "PBMC" & sce_main$patient_id == "Ref" ~ "Ref",
+# )
+# colData(sce_main)$tissue_type_mod <- factor(colData(sce_main)$tissue_type_mod, levels = c("PBMC", "MPE", "Ref"))
+
+# load processed sce
+sce_main <- readRDS(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\sce_main_with_meta6_full_lin_clusters.rds)')
+
+# set batch levels in desired order
+# c("1_PBMC", "1_MPE", "1_Ref", 2_PBMC", "2_MPE", "2_Ref", etc.)
+batch_levels <- unlist(lapply(levels(sce_main$BATCH), function(b) c(paste0(b, "_PBMC"), paste0(b, "_MPE"), paste0(b, "_Ref"))))
+
+# ------------------------------------------------------------------------------
+q_thres <- 0.05
+
+# for each TIER 2 cluster group
+for (i in seq_along(DE_files)) {
+  # read in table
+  DE <- read_table(DE_files[i])
+  
+  # filter for differentially expressed markers for each TIER 2 cluster
+  sig_DE <- DE %>%
+    dplyr::filter(p_adj_DS < q_thres)
+  
+  # if no significant markers
+  if (nrow(sig_DE) == 0) {
+    message("No significant markers: ", basename(DE_files[i]), "skipping...\n")
+    next
+  }
+  
+  # list of significant markers for every TIER 2 cluster
+  sig_marker_list <- split(sig_DE$marker, sig_DE$cluster)
+  
+  message("Processing sce_main & sce_sub...\n")
+  
+  # read TIER 2 rds
+  sce_sub <- readRDS(rds_files[i])
+  
+  tier2_name <- gsub("_sce_subclust.*", "", basename(rds_files[i])) # C4, C5, C6, Myeloid
+  sce_sub[[metak[[tier2_name]]]] <- cluster_ids(sce_sub, metak[[tier2_name]])
+  
+  sce_main_sub <- filterSCE(sce_main, meta6_full_lin %in% clust_group[[tier2_name]])
+  sce_main_sub[[metak[[tier2_name]]]] <- cluster_ids(sce_sub, metak[[tier2_name]])
+  
+  
+  # for each TIER 2 cluster that has significant marker, do violin plot
+  for (c in names(sig_marker_list)) {
+    sel_markers <- sig_marker_list[[c]]
+    message("DOING CLUSTER: ", c, "\n")
+    
+    # contains reference
+    sce_tmp_ref <- filterSCE(sce_main_sub, sce_main_sub[[metak[[tier2_name]]]] == as.numeric(c))
+    # no reference
+    sce_tmp <- filterSCE(sce_tmp_ref, patient_id != "Ref")
+    
+    sce_tmp_ref$BATCH_tissue <- factor(
+      paste0(sce_tmp_ref$BATCH, "_", sce_tmp_ref$tissue_type_mod),
+      levels = batch_levels
+    )
+    sce_tmp$BATCH_tissue <- factor(
+      paste0(sce_tmp$BATCH, "_", sce_tmp$tissue_type_mod),
+      levels = batch_levels
+    )
+    # sce_tmp has no ref cells
+    sce_tmp$BATCH_tissue <- droplevels(sce_tmp$BATCH_tissue)
+    
+  
+    # PBMC vs MPE --------------------------------------------------------------
+    message("PLOTTING: PBMC vs MPE - ", tier2_name, "-C", c, "\n")
+    
+    violin_PLOT <- scater::plotExpression(
+      sce_tmp,
+      features = sel_markers,
+      x = "tissue_type",
+      colour_by = "tissue_type",
+      exprs_values = "exprs",
+      scales = "free",
+      one_facet = TRUE,
+      scattermore = TRUE,
+      point_size = 1
+    ) +
+      ggtitle(paste0("Cytokines", " - Marker Exprs by Tissue Type; q < ", q_thres)) +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 0, hjust = 1))
+    
+    plot_height <- 1.6 * length(sel_markers)
+    
+    file_name <- file.path(output_dir, paste0("Cytokines_", tier2_name, "_C", c, "_tissue.png"))
+    ggsave(filename = file_name, plot = violin_PLOT, width = 10, height = plot_height, dpi = 300)
+    
+    
+    # PBMC vs MPE w/BATCH ------------------------------------------------------
+    message("PLOTTING: PBMC vs MPE w/BATCH - ", tier2_name, "-C", c, "\n")
+    
+    violin_PLOTS_BATCH <- lapply(sel_markers, function(m) {
+      scater::plotExpression(
+        sce_tmp,
+        features = m,
+        x = "BATCH_tissue",
+        colour_by = "tissue_type",
+        exprs_values = "exprs",
+        scales = "free",
+        one_facet = TRUE,
+        scattermore = TRUE,
+        point_size = 1
+      ) +
+        scale_x_discrete(labels = function(x) gsub("_.*", "", x)) +
+        ggtitle(paste0(m, " - Cytokines ", tier2_name, "-C", c, "; q < ", q_thres)) +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle = 0, hjust = 1))
+    })
+    final_violin_PLOT <- cowplot::plot_grid(plotlist = violin_PLOTS_BATCH, ncol = 1, labels = "AUTO")
+    
+    file_name <- file.path(output_dir, paste0("Cytokines_", tier2_name, "_C", c, "_tissue_batch.png"))
+    ggsave(filename = file_name, plot = final_violin_PLOT, width = 12, height = 4 * length(sel_markers), dpi = 300)
+    
+    
+    # PBMC vs MPE vs Ref -------------------------------------------------------
+    message("PLOTTING: PBMC vs MPE vs Ref - ", tier2_name, "-C", c, "\n")
+    
+    violin_PLOT <- scater::plotExpression(
+      sce_tmp_ref,
+      features = sel_markers,
+      x = "tissue_type_mod",
+      colour_by = "tissue_type_mod",
+      exprs_values = "exprs",
+      scales = "free",
+      one_facet = TRUE,
+      scattermore = TRUE,
+      point_size = 1
+    ) +
+      ggtitle(paste0("Cytokines", " - Marker Exprs by Tissue Type; q < ", q_thres)) +
+      theme_bw() +
+      theme(axis.text.x = element_text(angle = 0, hjust = 1))
+    
+    file_name <- file.path(output_dir, paste0("Cytokines_", tier2_name, "_C", c, "_tissue_ref.png"))
+    ggsave(filename = file_name, plot = violin_PLOT, width = 10, height = plot_height, dpi = 300)
+    
+    
+    # PBMC vs MPE vs Ref w/BATCH -----------------------------------------------
+    message("PLOTTING: PBMC vs MPE vs Ref w/BATCH - ", tier2_name, "-C", c, "\n")
+    
+    violin_PLOTS_BATCH <- lapply(sel_markers, function(m) {
+      scater::plotExpression(
+        sce_tmp_ref,
+        features = m,
+        x = "BATCH_tissue",
+        colour_by = "tissue_type_mod",
+        exprs_values = "exprs",
+        scales = "free",
+        one_facet = TRUE,
+        scattermore = TRUE,
+        point_size = 1
+      ) +
+        scale_x_discrete(labels = function(x) gsub("_.*", "", x)) +
+        ggtitle(paste0(m, " - Cytokines ", tier2_name, "-C", c, "; q < ", q_thres)) +
+        theme_bw() +
+        theme(axis.text.x = element_text(angle = 0, hjust = 1))
+    })
+    final_violin_PLOT <- cowplot::plot_grid(plotlist = violin_PLOTS_BATCH, ncol = 1, labels = "AUTO")
+    
+    file_name <- file.path(output_dir, paste0("Cytokines_", tier2_name, "_C", c, "_tissue_batch_ref.png"))
+    ggsave(filename = file_name, plot = final_violin_PLOT, width = 12, height = 4 * length(sel_markers), dpi = 300)
+  }
+}
---
+++ cytof-pipeline/MPE_Cytof/Rscript/Myeloid_Violin_Plots.R
@@ -0,0 +1,192 @@
+library(cowplot)
+
+output_dir <- r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Myeloid)'
+
+# LOAD DATA
+DE_c5 <- read_table(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\Results_Subcluster\Diff_Expr\Cytokines_C5_meta5_DE_table.txt)')
+
+# ------------------------------------------------------------------------------
+# 1. Filter for significance with p_adj_DS < 0.05, in cluster 3
+# 2. Get associated markers
+# 3. Violin plot
+#       - For each marker
+#           - pbmc vs mpe
+#           - pmbc vs mpe, facet by batch
+# ------------------------------------------------------------------------------
+
+# prepare SCE
+sce_main <- readRDS(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\sce_main_with_meta6_full_lin_clusters.rds)')
+# set pbmc as reference
+colData(sce_main)$tissue_type <- factor(colData(sce_main)$tissue_type, levels = c("PBMC", "MPE"))
+# add column denoting PBMC, MPE, or Ref for plotting
+colData(sce_main)$tissue_type_mod <- case_when(
+  sce_main$tissue_type == "MPE" ~ "MPE",
+  sce_main$tissue_type == "PBMC" & sce_main$patient_id != "Ref" ~ "PBMC",
+  sce_main$tissue_type == "PBMC" & sce_main$patient_id == "Ref" ~ "Ref",
+)
+colData(sce_main)$tissue_type_mod <- factor(colData(sce_main)$tissue_type_mod, levels = c("PBMC", "MPE", "Ref"))
+
+
+############################ C5 - C3 ###########################################
+# select markers
+sel_panel <- "Cytokines"
+q_thres <- 0.05
+
+sel_DE_c5 <- DE_c5 %>%
+  dplyr::filter(cluster == 3, p_adj_DS < q_thres)
+sel_markers <- sel_DE_c5$marker
+
+# ------------------------------------------------------------------------------
+
+
+sce_c5 <- readRDS(r'(D:\CHRISTOPHER_BUI\MPE_CYTOF_RDS\Cytokines\Results_Subcluster\RDS_Subcluster\C5_sce_subclust_full_lineage.rds)')
+sce_c5$meta5 <- cluster_ids(sce_c5, "meta5")
+
+# need sce with all markers
+sce_main_c5 <- filterSCE(sce_main, meta6_full_lin == 5)
+sce_main_c5$meta5 <- sce_c5$meta5
+
+sce_tmp_ref <- filterSCE(sce_main_c5, meta5 == 3)
+sce_tmp <- filterSCE(sce_tmp_ref, patient_id != "Ref")
+
+# set batch levels in desired order
+# c("1_PBMC", "1_MPE", "1_Ref", 2_PBMC", "2_MPE", "2_Ref", etc.)
+batch_levels <- unlist(lapply(levels(sce_main_c5$BATCH), function(b) c(paste0(b, "_PBMC"), paste0(b, "_MPE"), paste0(b, "_Ref"))))
+
+sce_tmp_ref$BATCH_tissue <- factor(
+  paste0(sce_tmp_ref$BATCH, "_", sce_tmp_ref$tissue_type_mod),
+  levels = batch_levels
+)
+sce_tmp$BATCH_tissue <- factor(
+  paste0(sce_tmp$BATCH, "_", sce_tmp$tissue_type_mod),
+  levels = batch_levels
+)
+# sce_tmp has no Ref cells
+sce_tmp$BATCH_tissue <- droplevels(sce_tmp$BATCH_tissue)
+
+
+# PBMC vs MPE ------------------------------------------------------------------
+violin_PLOT <- scater::plotExpression(
+  sce_tmp,
+  features = sel_markers,
+  x = "tissue_type",
+  colour_by = "tissue_type",
+  exprs_values = "exprs",
+  scales = "free",
+  one_facet = TRUE,
+  scattermore = TRUE,
+  point_size = 1
+) +
+  ggtitle(paste0(sel_panel, " - Marker Exprs by Tissue Type; q < ", q_thres)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1))
+
+file_name <- file.path(output_dir, "Cytokines_C5_C3_sig_markers_by_tissue.png")
+ggsave(filename = file_name, plot = violin_PLOT, width = 10, height = 14, dpi = 300)
+
+
+# PBMC vs MPE w/BATCH ----------------------------------------------------------
+violin_PLOTS_BATCH <- lapply(sel_markers, function(m) {
+  scater::plotExpression(
+    sce_tmp,
+    features = m,
+    x = "BATCH_tissue",
+    colour_by = "tissue_type",
+    exprs_values = "exprs",
+    scales = "free",
+    one_facet = TRUE,
+    scattermore = TRUE,
+    point_size = 1
+  ) +
+    scale_x_discrete(labels = function(x) gsub("_.*", "", x)) +
+    ggtitle(paste0(m, " - Cytokines C5-C3; q < ", q_thres)) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1))
+})
+
+final_violin_plot <- cowplot::plot_grid(plotlist = violin_PLOTS_BATCH, ncol = 1, labels = "AUTO")
+
+file_name <- file.path(output_dir, "Cytokines_C5_C3_sig_markers_by_tissue_batch.png")
+ggsave(filename = file_name, plot = final_violin_plot, width = 12, height = 4 * length(sel_markers), dpi = 300)
+
+
+# PBMC vs MPE vs Ref -----------------------------------------------------------
+violin_PLOT <- scater::plotExpression(
+  sce_tmp_ref,
+  features = sel_markers,
+  x = "tissue_type_mod",
+  colour_by = "tissue_type_mod",
+  exprs_values = "exprs",
+  scales = "free",
+  one_facet = TRUE,
+  scattermore = TRUE,
+  point_size = 1
+) +
+  ggtitle(paste0(sel_panel, " - Marker Exprs by Tissue Type; q < ", q_thres)) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1))
+
+file_name <- file.path(output_dir, "Cytokines_C5_C3_sig_markers_by_tissue_REF.png")
+ggsave(filename = file_name, plot = violin_PLOT, width = 12, height = 14, dpi = 300)
+
+
+# PBMC vs MPE vs Ref w/BATCH ---------------------------------------------------
+violin_PLOTS_BATCH <- lapply(sel_markers, function(m) {
+  scater::plotExpression(
+    sce_tmp_ref,
+    features = m,
+    x = "BATCH_tissue",
+    colour_by = "tissue_type_mod",
+    exprs_values = "exprs",
+    scales = "free",
+    one_facet = TRUE,
+    scattermore = TRUE,
+    point_size = 1
+  ) +
+    scale_x_discrete(labels = function(x) gsub("_.*", "", x)) +
+    ggtitle(paste0(m, " - Cytokines C5-C3; q < ", q_thres)) +
+    theme_bw() +
+    theme(axis.text.x = element_text(angle = 0, hjust = 1))
+})
+
+final_violin_plot <- cowplot::plot_grid(plotlist = violin_PLOTS_BATCH, ncol = 1, labels = "AUTO")
+
+file_name <- file.path(output_dir, "Cytokines_C5_C3_sig_markers_by_tissue_batch_REF.png")
+ggsave(filename = file_name, plot = final_violin_plot, width = 16, height = 4 * length(sel_markers), dpi = 300)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ADJUST BELOW FOR BETTER TITLE; USE ABOVE FOR NOW
+# final_plot <- plot_grid(
+#   ggdraw() +
+#     draw_label(paste0(sel_panel, " - Marker Exprs by Batch & Tissue Type; p < ", p_thres),
+#                hjust = 0.5,
+#                size = 18),
+#   big_violin_plot,
+#   ncol = 1,
+#   rel_heights = c(0.05, 1)
+# )
