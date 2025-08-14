969235a888d86e2c8a82f12e777bd469071155fc
Christopher Bui <christopherbui@pm.me>
2025-08-14 16:55:17 -0700

    updates

--- CIRM_MIF/analysis.R
+++ CIRM_MIF/analysis.R
@@ -3,6 +3,9 @@ library(dplyr)
 ################################################################
 # PANEL 2, SET 1
 ################################################################
+# output directory
+output_dir <- r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\Output)'
+
 # folder with input files
 inputParentFolder <- r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\20241121 CIRM Panel 2 Set 1 analysis\20241121 CIRM Panel 2 Set 1 analysis ObjectData)'
 
@@ -15,34 +18,66 @@ tmps <- list.files(path=tmpin, pattern="object_results.csv", full.names=FALSE)
 summary_file <- paste0(r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\20241121 CIRM Panel 2 Set 1 analysis\20241121 CIRM Panel 2 Set 1 analysis Total_Summary_Results.csv)')
 data_summary <- read.csv(summary_file, stringsAsFactors = FALSE, header = TRUE)
 
+# output file
+fout <- c("listHaloOut_panel2_batch1_08142025.txt")
+
 # extract meta data from file names
 tmps_cond1 <- t(sapply(tmps, function(x) unlist(strsplit(x, split="\\."))[1:2]))
 
+# extract slide ID: different format from first two batches and others
+##tmps_slideID <- t(sapply(tmps_cond1[,1],function(x)
+##                   unlist(strsplit(x,split=" "))[3:4]))
+tmps_slideID <- matrix(NA,nrow(tmps_cond1),2)
+for (i in c(1:nrow(tmps_cond1))){
+  tmp_str <- unlist(strsplit(tmps_cond1[i,1],split=" ")) 
+  if (length(tmp_str)>2){
+    tmps_slideID[i,] <- tmp_str[3:4]   #*****
+  }else{
+    tmps_slideID[i,] <- tmp_str
+  }
+}
+rownames(tmps_slideID) <- NULL
 
-# CHECK IF EACH FILE NAME MATCHES DATA IN SUMMARY FILE ----------------
+tmps_jobID <- sapply(tmps_cond1[,2],function(x)
+  substr(x,5,11))
 
-# image tag
-img_tag <- paste0(tmps_cond1[, 1], ".tif")
-match_imgtag <- img_tag %in% data_summary$Image.Tag
-
-# job id
-job_id <- sapply(tmps_cond1[, 2], function(x) {
-  sub(".*_job(\\d+)CIRM.*", "\\1", x)
-})
-match_jobid <- job_id %in% data_summary$Job.Id
-
-# algorithm name
-algo_name <- sapply(tmps_cond1[, 2], function(x) {
-  sub(".*(CIRM.*)_object_results.*", "\\1", x)
-})
-match_algo <- algo_name %in% data_summary$Algorithm.Name
-
-print(paste0(sum(match_imgtag), "/", length(match_imgtag), " Files Matched"))
-print(paste0(sum(match_jobid), "/", length(match_jobid), " Files Matched"))
-print(paste0(sum(match_algo), "/", length(match_algo), " Files Matched"))
+names(tmps_jobID) <- NULL
 
 
-# CHECK IF COLUMN NAMES MATCH ------------------------------------------
+nfiles <- length(tmpf)
+listFiles <- NULL
+roi_column_interest <- c("Analysis.Region")
+
+for (j in c(1:nfiles)){
+  tmp_data_in <- read.csv(tmpf[j],stringsAsFactors=FALSE,header=T)
+  image_colnames <- colnames(tmp_data_in)
+  if (is.element(roi_column_interest,image_colnames)){
+    roi_info <- table(tmp_data_in[,roi_column_interest])
+    n_roi <- length(roi_info)
+    # check existence of ROI in the summary file
+    is_sum_avai <- is.element(names(roi_info),data_summary$Analysis.Region)
+    tmp4sum <- ifelse(is_sum_avai==TRUE,summary_file,"NA")
+    tmp_info <- cbind(rep(tmpf[j],n_roi),
+                      rep(tmps_slideID[j,1],n_roi),
+                      rep(tmps_slideID[j,2],n_roi),
+                      rep(tmps_jobID[j],n_roi),
+                      names(roi_info),as.numeric(roi_info),
+                      is_sum_avai,tmp4sum)
+    listFiles <-  rbind(listFiles,tmp_info)
+    
+  } else {
+    stop("roi_column_interest not found in image")
+  }
+}
+
+colnames(listFiles) <- c("originalFileName","PathologicalID","BlockID",
+                         "jobID","originalROI","cellNumber","IsSummaryAvai","SumFileName")
+
+write.table(listFiles, file.path(output_dir, fout), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+
+
+# CHECK IF COLUMN NAMES MATCH WITHIN BATCH ------------------------------------------
 
 colnames_list <- list()
 file_names <- basename(tmpf)
@@ -78,19 +113,14 @@ for (i in names(sel_cols_check)) {
   }
 }
 
-# TEST RUN TEMPLATE SCRIPT ------------------------------------
-tmpin <- inputParentFolder
-tmpf <- list.files(path=tmpin, pattern="object_results", full.names=TRUE)
-tmps <- list.files(path=tmpin, pattern="object_results", full.names=FALSE)
-
-fin_summary_file
-
-
 
 
 ################################################################
 # PANEL 2, SET 2
 ################################################################
+# output directory
+output_dir <- r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\Output)'
+
 # folder with input files
 inputParentFolder <- r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\20241121 CIRM Panel 2 Set 2 analysis\20241121 CIRM Panel 2 Set 2 analysis ObjectData)'
 
@@ -103,34 +133,66 @@ tmps <- list.files(path=tmpin, pattern="object_results.csv", full.names=FALSE)
 summary_file <- paste0(r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\20241121 CIRM Panel 2 Set 2 analysis\20241121 CIRM Panel 2 Set 2 analysis.csv)')
 data_summary <- read.csv(summary_file, stringsAsFactors = FALSE, header = TRUE)
 
+# output file
+fout <- c("listHaloOut_panel2_batch2_08142025.txt")
+
 # extract meta data from file names
 tmps_cond1 <- t(sapply(tmps, function(x) unlist(strsplit(x, split="\\."))[1:2]))
 
+# extract slide ID: different format from first two batches and others
+##tmps_slideID <- t(sapply(tmps_cond1[,1],function(x)
+##                   unlist(strsplit(x,split=" "))[3:4]))
+tmps_slideID <- matrix(NA,nrow(tmps_cond1),2)
+for (i in c(1:nrow(tmps_cond1))){
+  tmp_str <- unlist(strsplit(tmps_cond1[i,1],split=" ")) 
+  if (length(tmp_str)>2){
+    tmps_slideID[i,] <- tmp_str[3:4]   #*****
+  }else{
+    tmps_slideID[i,] <- tmp_str
+  }
+}
+rownames(tmps_slideID) <- NULL
 
-# CHECK IF EACH FILE NAME MATCHES DATA IN SUMMARY FILE ----------------
+tmps_jobID <- sapply(tmps_cond1[,2],function(x)
+  substr(x,5,11))
 
-# image tag
-img_tag <- paste0(tmps_cond1[, 1], ".tif")
-match_imgtag <- img_tag %in% data_summary$Image.Tag
-
-# job id
-job_id <- sapply(tmps_cond1[, 2], function(x) {
-  sub(".*_job(\\d+)CIRM.*", "\\1", x)
-})
-match_jobid <- job_id %in% data_summary$Job.Id
-
-# algorithm name
-algo_name <- sapply(tmps_cond1[, 2], function(x) {
-  sub(".*(CIRM.*)_object_results.*", "\\1", x)
-})
-match_algo <- algo_name %in% data_summary$Algorithm.Name
-
-print(paste0(sum(match_imgtag), "/", length(match_imgtag), " Files Matched"))
-print(paste0(sum(match_jobid), "/", length(match_jobid), " Files Matched"))
-print(paste0(sum(match_algo), "/", length(match_algo), " Files Matched"))
+names(tmps_jobID) <- NULL
 
 
-# CHECK IF COLUMN NAMES MATCH ------------------------------------------
+nfiles <- length(tmpf)
+listFiles <- NULL
+roi_column_interest <- c("Analysis.Region")
+
+for (j in c(1:nfiles)){
+  tmp_data_in <- read.csv(tmpf[j],stringsAsFactors=FALSE,header=T)
+  image_colnames <- colnames(tmp_data_in)
+  if (is.element(roi_column_interest,image_colnames)){
+    roi_info <- table(tmp_data_in[,roi_column_interest])
+    n_roi <- length(roi_info)
+    # check existence of ROI in the summary file
+    is_sum_avai <- is.element(names(roi_info),data_summary$Analysis.Region)
+    tmp4sum <- ifelse(is_sum_avai==TRUE,summary_file,"NA")
+    tmp_info <- cbind(rep(tmpf[j],n_roi),
+                      rep(tmps_slideID[j,1],n_roi),
+                      rep(tmps_slideID[j,2],n_roi),
+                      rep(tmps_jobID[j],n_roi),
+                      names(roi_info),as.numeric(roi_info),
+                      is_sum_avai,tmp4sum)
+    listFiles <-  rbind(listFiles,tmp_info)
+    
+  } else {
+    stop("roi_column_interest not found in image")
+  }
+}
+
+colnames(listFiles) <- c("originalFileName","PathologicalID","BlockID",
+                         "jobID","originalROI","cellNumber","IsSummaryAvai","SumFileName")
+
+write.table(listFiles, file.path(output_dir, fout), sep="\t", quote=FALSE, row.names=FALSE, col.names=TRUE)
+
+
+
+# CHECK IF COLUMN NAMES MATCH WITHIN BATCH ------------------------------------------
 
 colnames_list <- list()
 file_names <- basename(tmpf)
@@ -166,4 +228,24 @@ for (i in names(sel_cols_check)) {
   }
 }
 
+################################################################
+# CHECK IF COLUMN NAMES MATCH BETWEEN BATCHES
+################################################################
+inputFolderP2B1 <- r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\20241121 CIRM Panel 2 Set 1 analysis\20241121 CIRM Panel 2 Set 1 analysis ObjectData)'
+inputFolderP2B2 <- r'(D:\CHRISTOPHER_BUI\CIRM_MIF\Data\20241121 CIRM Panel 2 Set 2 analysis\20241121 CIRM Panel 2 Set 2 analysis ObjectData)'
 
+tmpf1 <- list.files(inputFolderP2B1, pattern="object_results.csv", full.names=TRUE)
+tmpf2 <- list.files(inputFolderP2B2, pattern="object_results.csv", full.names=TRUE)
+
+column_names1 <- colnames(read.csv(tmpf1[1], stringsAsFactors=FALSE, check.names=FALSE))
+column_names2 <- colnames(read.csv(tmpf2[1], stringsAsFactors=FALSE, check.names=FALSE))
+
+batch_cols_check <- identical(column_names1, column_names2)
+print(batch_cols_check)
+
+
+
+
+################################################################
+# LEVEL 3 REGENERATION
+################################################################
